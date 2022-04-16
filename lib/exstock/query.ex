03ba3e5c0c2d284566ac0ea54c86af0f677d76aa
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2022-04-16 19:14:00 -0300

    Refact query to support crypto

--- lib/exstock/query.ex
+++ lib/exstock/query.ex
@@ -2,10 +2,24 @@ defmodule Exstock.Query do
   @apiurl Application.get_env(:exstock, :finance_apiurl)
   @apikey Application.get_env(:exstock, :finance_apikey)
 
-  def get_quote(symbol) do
+  def quote(symbol) do
     url_for(symbol)
     |> HTTPoison.get([{"Accept", "application/json"}, {"X-API-KEY", @apikey}])
     |> parse_response
+    |> process_quote()
+  end
+
+  def recommendation_trend(symbol) do
+    "#{@apiurl}/v11/finance/quoteSummary/#{symbol}?lang=en&region=US&modules=recommendationTrend"
+    |> HTTPoison.get([{"Accept", "application/json"}, {"X-API-KEY", @apikey}])
+    |> parse_response()
+    |> process_quote()
+  end
+
+  def summary_price(symbol) do
+    "#{@apiurl}/v11/finance/quoteSummary/#{symbol}?lang=en&region=US&modules=price"
+    |> HTTPoison.get([{"Accept", "application/json"}, {"X-API-KEY", @apikey}])
+    |> parse_response()
   end
 
   defp url_for(symbol, params \\ [region: "US", lang: "en"]) do
@@ -19,14 +33,53 @@ defmodule Exstock.Query do
   end
 
   defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
-    body |> JSON.decode!() |> process_quote
+    body |> JSON.decode!()
   end
 
-  defp process_quote(json) do
+  defp parse_response({:error, response}) do
+    {:error, response}
+  end
+
+  defp process_quote(%{"quoteResponse" => response}) do
     try do
-      {:ok, json["quoteResponse"]["result"]}
+      data = response["result"] |> List.first()
+      {:ok, process_fields(data)}
     rescue
-      _ -> :error
+      exception -> {:error, exception}
     end
+  end
+
+  defp process_quote(%{"quoteSummary" => response}) do
+    try do
+      data = response["result"] |> List.first()
+      {:ok, data}
+    rescue
+      exception -> {:error, exception}
+    end
+  end
+
+  defp process_fields(data = %{"quoteType" => "EQUITY"}) do
+    %{
+      "regularMarketDayHigh" => high,
+      "regularMarketDayLow" => low,
+      "postMarketPrice" => closePrice,
+      "regularMarketPrice" => price,
+      "shortName" => name,
+      "quoteType" => type
+    } = data
+
+    %{high: high, low: low, closePrice: closePrice, price: price, name: name, type: type}
+  end
+
+  defp process_fields(data = %{"quoteType" => "CRYPTOCURRENCY"}) do
+    %{
+      "regularMarketDayHigh" => high,
+      "regularMarketDayLow" => low,
+      "regularMarketPrice" => price,
+      "shortName" => name,
+      "quoteType" => type
+    } = data
+
+    %{high: high, low: low, price: price, name: name, type: type}
   end
 end

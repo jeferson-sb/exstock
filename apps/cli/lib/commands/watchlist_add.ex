d578fe1f877cdb793f2201849c96fc9a5d002a27
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2025-01-28 19:34:54 -0300

    feat(cli): fix wallet:add for range conditions

--- apps/cli/lib/commands/watchlist_add.ex
+++ apps/cli/lib/commands/watchlist_add.ex
@@ -3,7 +3,7 @@ defmodule Commands.WatchlistAdd do
 
   @impl true
   def execute([symbol: symbol, condition: condition, portfolio_id: portfolio_id]) do
-    [left, right] = condition |> String.trim |> String.split(" ")
+    [left | right] = condition |> String.trim |> String.split(" ")
     Tracker.Watchlist.Repo.create(%{symbol: symbol, condition: %{String.to_atom(left) => right}, portfolio_id: portfolio_id})
   end
 end
--- apps/tracker/lib/tracker/watchlist/watcher.ex
+++ apps/tracker/lib/tracker/watchlist/watcher.ex
@@ -1,7 +1,7 @@
 defmodule Tracker.Watcher do
   def watch(%{"up" => target}, price) when price > target, do: {:up, target}
   def watch(%{"down" => target}, price) when price < target, do: {:down, target}
-  def watch(%{"range" => [upper, lower]}, price) when price <= upper and price >= lower,
+  def watch(%{"range" => [lower, upper]}, price) when price <= upper and price >= lower,
     do: {:range, lower, upper}
 
   def watch(%{"percentage_up" => target}, price) when price > price * target + price,
